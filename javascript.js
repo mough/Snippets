61b98577d28ca45f23abeb9832fdc4074caa8e2e
mough <mmslavin@gmail.com>
2022-08-12 17:46:48 -0500

    Creating javascript.js
    adding js snippets

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// iterate over object
+for (const [key, value] of Object.entries(object)) {
+  console.log(key, value);
+}
+
+// parse int
+// https://www.w3schools.com/jsref/jsref_parseint.asp
+parseInt("10");
+let val = parseInt(value); // NaN
+ if (!val)
