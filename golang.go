82dd3ef0cce9ea65cb528cd5cc15f47f9ae47d38
mough <mmslavin@gmail.com>
2022-11-13 17:12:33 -0600

    Update golang.go

--- golang.go
+++ golang.go
@@ -1,3 +1,30 @@
+// safe dictionary lookup
 if val, ok := dict["foo"]; ok {
     //do something here
 }
+
+// safe casting
+firstString, ok := firstVar.(string)
+if (!ok) {
+	fmt.Printf("firstString is not a string, do something about it! Value:'%v'\n", firstString)
+}
+
+// new guid
+import (
+    "github.com/google/uuid"
+)
+uuidWithHyphen := uuid.New
+
+// get keys from dictionary
+keys := make([]int, len(mymap))
+i := 0
+for k := range mymap {
+    keys[i] = k
+    i++
+}
+
+// random int
+import (
+    "math/rand"
+)
+rand.Intn(100)
