a747035700f99583b3b17c59382dd1519084e257
mough <mmslavin@gmail.com>
2022-11-26 11:39:19 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,9 +2,20 @@
 for (const [key, value] of Object.entries(object)) {
   console.log(key, value);
 }
+// number of keys in object
+Object.keys(ret).length
 
 // parse int
 // https://www.w3schools.com/jsref/jsref_parseint.asp
 parseInt("10");
 let val = parseInt(value); // NaN
  if (!val)
+
+// trim whitespace
+someString.trim();
+
+// if key in object
+object.hasOwnProperty('key')
+
+// add item to end of array
+someArray.push("someValue");
